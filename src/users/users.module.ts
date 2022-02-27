93df91b5db1bb7f21afd90ebda56e62ac64a8988
Bastodon <71769609+Bastodon@users.noreply.github.com>
2022-02-27 17:56:27 +0100

    Commit serveur
    Commit des fichiers sources du serveur

---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+
+@Module(
+{
+  imports: [DatabaseModule],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
+})
+export class UsersModule {}
